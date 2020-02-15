82e67e5ce242e75b874a984b966172e928563230
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-02-14 22:06:43 -0300

    feat: migration teacher

---
+++ src/database/migrations/20200215005104-create-table-teacher.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('teacher', {
+      id_teacher: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+      department_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'department', key: 'id_department' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: queryInterface => queryInterface.dropTable('teacher'),
+};
