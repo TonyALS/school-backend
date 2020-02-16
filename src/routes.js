5d8fcb68858e33d11ff935f4d31f7a5c168bd2fd
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-02-16 14:37:26 -0300

    feat: create routes store and list teachers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import DepartmentController from './app/controllers/DepartmentController';
+import TeacherController from './app/controllers/TeacherController';
 
 const routes = new Router();
 
-routes.post('/department', DepartmentController.store);
-routes.get('/department', DepartmentController.index);
+routes.post('/departments', DepartmentController.store);
+routes.get('/departments', DepartmentController.index);
+
+routes.post('/teachers', TeacherController.store);
+routes.get('/teachers', TeacherController.index);
 
 export default routes;
