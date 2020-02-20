1e08a1c9913ccc06583f0113e9e397b5ee18db40
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-02-19 21:42:40 -0300

    refactor: rename functions and implements validators

--- src/app/controllers/DepartmentController.js
+++ src/app/controllers/DepartmentController.js
@@ -1,5 +1,6 @@
-import * as Yup from 'yup';
 import Department from '../models/Department';
+import Teacher from '../models/Teacher';
+import Course from '../models/Course';
 
 class DepartmentContoller {
   async index(req, res) {
@@ -11,17 +12,49 @@ class DepartmentContoller {
     }
   }
 
-  async store(req, res) {
-    const schema = Yup.object().shape({
-      department_name: Yup.string()
-        .required()
-        .min(6),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+  //  Listar todos os professores de um departmento:
+  async getTeacherByDepartment(req, res) {
+    try {
+      const teacher = await Teacher.findAndCountAll({
+        where: {
+          department_id: req.params.id_department,
+        },
+        attributes: ['first_name', 'status'],
+        include: [
+          {
+            association: 'department',
+            attributes: ['department_name'],
+          },
+        ],
+      });
+      return res.status(200).json(teacher);
+    } catch (erro) {
+      return res.status(500).json(erro);
     }
+  }
 
+  //  Listar todos os cursos de um departmento:
+  async getCourseByDepartment(req, res) {
+    try {
+      const course = await Course.findAndCountAll({
+        where: {
+          department_id: req.params.id_department,
+        },
+        attributes: ['course_name', 'mec_authorization'],
+        include: [
+          {
+            association: 'department',
+            attributes: ['department_name'],
+          },
+        ],
+      });
+      return res.status(200).json(course);
+    } catch (erro) {
+      return res.status(500).json(erro);
+    }
+  }
+
+  async store(req, res) {
     try {
       const department = await Department.create(req.body);
       return res.json(department);
--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,33 @@ import DepartmentController from './app/controllers/DepartmentController';
 import TeacherController from './app/controllers/TeacherController';
 import CourseController from './app/controllers/CourseController';
 
+//  Middlewares
+import validateCourseStore from './app/validators/CourseStore';
+import validateTeacherStore from './app/validators/TeacherStore';
+
 const routes = new Router();
 
 routes.post('/departments', DepartmentController.store);
 routes.get('/departments', DepartmentController.index);
-routes.put('/departments/:id_department', DepartmentController.update);
+routes.put('/departments/update/:id_department', DepartmentController.update);
+routes.get(
+  '/departments/:id_department/teachers',
+  DepartmentController.getTeacherByDepartment
+);
+routes.get(
+  '/departments/:id_department/courses',
+  DepartmentController.getCourseByDepartment
+);
 
-routes.post('/teachers', TeacherController.store);
+routes.post('/teachers', validateTeacherStore, TeacherController.store);
 routes.get('/teachers', TeacherController.index);
-routes.put('/teachers/:id_teacher', TeacherController.update);
+routes.put(
+  '/teachers/:id_teacher',
+  validateTeacherStore,
+  TeacherController.update
+);
 
-routes.post('/courses', CourseController.store);
+routes.post('/courses', validateCourseStore, CourseController.store);
 routes.get('/courses', CourseController.index);
 
 export default routes;
