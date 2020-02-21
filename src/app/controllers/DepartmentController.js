b6a2d0463bedcca62b03a868dc5831365298ad09
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-02-20 23:20:38 -0300

    refactor: update departmentcontroller methods

--- src/app/controllers/DepartmentController.js
+++ src/app/controllers/DepartmentController.js
@@ -1,57 +1,53 @@
 import Department from '../models/Department';
-import Teacher from '../models/Teacher';
-import Course from '../models/Course';
+import DepartmentListService from '../services/DepartmentListService';
 
 class DepartmentContoller {
+  //  Listar todos os departamentos:
   async index(req, res) {
-    try {
-      const departments = await Department.findAll();
-      return res.json(departments);
-    } catch (erro) {
-      return res.status(500).json(erro);
+    const departments = await DepartmentListService.getAllDepartments();
+
+    if (departments.count === 0) {
+      return res.status(400).json({ error: 'Nenhum departamento encontrado' });
     }
+
+    return res.status(200).json(departments);
   }
 
   //  Listar todos os professores de um departmento:
-  async getTeacherByDepartment(req, res) {
-    try {
-      const teacher = await Teacher.findAndCountAll({
-        where: {
-          department_id: req.params.id_department,
-        },
-        attributes: ['first_name', 'status'],
-        include: [
-          {
-            association: 'department',
-            attributes: ['department_name'],
-          },
-        ],
-      });
-      return res.status(200).json(teacher);
-    } catch (erro) {
-      return res.status(500).json(erro);
+  async ListTeacherByDepartment(req, res) {
+    const department_id = req.params.id_department;
+
+    if (
+      !(await DepartmentListService.searchDepartmentById({ department_id }))
+    ) {
+      return res.status(400).json({ error: 'Departamento não encontrado' });
     }
+
+    const teacherByDepartment = await DepartmentListService.getTeacherByDepartment(
+      {
+        department_id,
+      }
+    );
+
+    return res.status(200).json(teacherByDepartment);
   }
 
   //  Listar todos os cursos de um departmento:
-  async getCourseByDepartment(req, res) {
-    try {
-      const course = await Course.findAndCountAll({
-        where: {
-          department_id: req.params.id_department,
-        },
-        attributes: ['course_name', 'mec_authorization'],
-        include: [
-          {
-            association: 'department',
-            attributes: ['department_name'],
-          },
-        ],
-      });
-      return res.status(200).json(course);
-    } catch (erro) {
-      return res.status(500).json(erro);
+  async ListCourseByDepartment(req, res) {
+    const department_id = req.params.id_department;
+
+    if (
+      !(await DepartmentListService.searchDepartmentById({ department_id }))
+    ) {
+      return res.status(400).json({ error: 'Departamento não encontrado' });
     }
+
+    const courseByDepartment = await DepartmentListService.getCourseByDepartment(
+      {
+        department_id,
+      }
+    );
+    return res.status(200).json(courseByDepartment);
   }
 
   async store(req, res) {
@@ -64,12 +60,20 @@ class DepartmentContoller {
   }
 
   async update(req, res) {
+    const department_id = req.params.id_department;
+
+    if (
+      !(await DepartmentListService.searchDepartmentById({ department_id }))
+    ) {
+      return res.status(400).json({ error: 'Departamento não encontrado' });
+    }
+
     try {
-      const department = await Department.findByPk(req.params.id_department);
+      const department = await Department.findByPk(department_id);
       await department.update(req.body);
-      return res.json(department);
+      return res.status(200).json(department);
     } catch (error) {
-      return res.json(error);
+      return res.status(400).json(error);
     }
   }
 }
