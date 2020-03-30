27b5556b7cdfd756ce64027d06f51f64c3d97a08
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-03-29 21:43:37 -0300

    refactor: update http response status codes

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -48,7 +48,7 @@ class CourseController {
         success: 'Cadastro realizado com sucesso',
       });
     } catch (error) {
-      return res.status(400).json(error);
+      return res.status(500).json(error);
     }
   }
 
@@ -75,7 +75,7 @@ class CourseController {
       });
     } catch (error) {
       if (error.name === 'SequelizeUniqueConstraintError') {
-        return res.status(500).json({
+        return res.status(400).json({
           error: 'Curso já cadastrado na plataforma',
         });
       }
